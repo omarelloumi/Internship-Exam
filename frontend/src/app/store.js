92b045e9c1da9bc7c3e7fab59c71f2197be579a8
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2022-08-06 15:00:44 +0100

    configure stagiaire slice redux

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import stagiaireSlice from '../features/stagiaire/stagiaireSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    stagiaire: stagiaireSlice,
+    counter: counterReducer
   },
 });
---
+++ frontend/src/features/stagiaire/stagiaireSlice.js
@@ -0,0 +1,63 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import stagiaireService from './stagiaireService';
+
+const initialState = {
+    stagiaires : [],
+    stagiaire :{}
+}
+
+const getStagiaires = createAsyncThunk('stagiaire/getStagiaires',async () => {
+    const response = await stagiaireService.getStagiaires();
+    return response
+})
+
+const getStagiaireById = createAsyncThunk('stagiaire/getStagiaireById', async (id) =>{
+    const response = await stagiaireService.getStagiaireById(id);
+    return response
+})
+
+const addStagiaire = createAsyncThunk('stagiaire/addStagiaire', async (stagiaire) => {
+    const response = await stagiaireService.addStagiaire(stagiaire)
+    return response
+})
+
+const updateStagiaire = createAsyncThunk('stagiaire/updateStagiaire', async(id,stagiaire) =>{
+    const response = await stagiaireService.updateStagiaire(id,stagiaire)
+    return response
+})
+
+const deleteStagiaire = createAsyncThunk('stagiaire/deleteStagiaire',async(id) => {
+    const response = await stagiaireService.deleteStagiaire(id)
+    return response
+})
+
+const stagiaireSlice = createSlice({
+    name : 'stagiaire',
+    initialState,
+    reducers :{
+        reset : (state)=> initialState
+    },
+    extraReducers : (builder) => {
+        builder
+            .addCase(getStagiaires.fulfilled, (state,action) => {
+                state.stagiaires = action.payload
+            })
+            .addCase(getStagiaireById.fulfilled, (state,action)=>{
+                state.stagiaire = action.payload
+            })
+            .addCase(addStagiaire.fulfilled, (state,action)=>{
+                state.stagiaires.push(action.payload)
+            })
+            .addCase(updateStagiaire.fulfilled, (state,action)=>{
+                const index = state.stagiaires.findIndex((stagiaire) => stagiaire._id === action.payload._id)
+                state.stagiaires[index]= action.payload
+            })
+            .addCase(deleteStagiaire.fulfilled, (state,action)=>{
+                state.stagiaires = state.stagiaires.filter((stagiaire) => stagiaire._id !== action.payload._id)
+            })
+    }
+})
+
+
+export const { reset } = stagiaireSlice.actions
+export default stagiaireSlice.reducer
