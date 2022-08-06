4fde5a4dbf7f57b563dbccbae2bfcf6cbb378fef
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2022-08-06 13:33:35 +0100

    connect Database

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require ('express');
 const dotenv = require('dotenv').config();
 const app = express();
+const mongoose = require('mongoose')
+const stagiaireRouter = require ('./backend/routes/stagiaireRoutes')
 
-
+mongoose.connect(process.env.URL)
 
 app.use(express.json())
 app.use(express.urlencoded({extended : false}));
+
+app.use('/api/stagiaires', stagiaireRouter);
 
 app.listen(process.env.PORT , ()=>{
     console.log("server running on port "+process.env.PORT)
