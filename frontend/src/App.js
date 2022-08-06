15f54766cf1a72a4ac49ceae5e2cd1a5003a74fb
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2022-08-06 16:01:44 +0100

    create Header components and routes
    fix stagiaire service

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-
+import Header from './components/Header'
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <Router>
+
+      <Header/>
+      <div className='container'>
+        
+      </div> 
+
+    </Router>
+    </>
   );
 }
 
--- frontend/src/features/stagiaire/stagiaireService.js
+++ frontend/src/features/stagiaire/stagiaireService.js
@@ -24,6 +24,7 @@ const updateStagiaire = async (id , stagiaire) => {
 
 const deleteStagiaire = async (id) =>{
     const response = await axios.delete(API+id)
+    return response.data
 }
 
 const stagiaireService = {
