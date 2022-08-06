0d8d7013d3f197262098630d21dd7642cf148f22
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2022-08-06 14:22:36 +0100

    create stagiaire service

---
+++ frontend/src/features/stagiaire/stagiaireService.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+
+const API= '/api/stagiaires/'
+
+const getStagiaires = async () => {
+    const response = await axios.get(API)
+    return response.data
+}
+
+const getStagiaireById = async(id) => {
+    const response = await axios.get(API+id)
+    return response.data
+}
+
+const addStagiaire = async (stagiaire) => {
+    const response = await axios.post(API,stagiaire)
+    return response.data
+}
+
+const updateStagiaire = async (id , stagiaire) => {
+    const response = await axios.put(API+id,stagiaire)
+    return response.data
+}
+
+const deleteStagiaire = async (id) =>{
+    const response = await axios.delete(API+id)
+}
+
+const stagiaireService = {
+    getStagiaires,
+    getStagiaireById,
+    addStagiaire,
+    updateStagiaire,
+    deleteStagiaire
+}
+
+export default stagiaireService
+
